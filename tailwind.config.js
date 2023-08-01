ae877239577e6ecc99c9a1e77e5e349c911dc1f2
Homayoun Norozy <homayounn77@gmail.com>
2023-08-01 16:32:21 +0330

    bug fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,8 @@ module.exports = {
     extend: {
       colors: {
         primary: '#0a192f',
-        gradient1: 'rgba(112,157,255,0.1)',
-        gradient2: 'hsla(242,74%,61%,0.1)'
+        gradient1: 'rgba(112, 157, 255, 0.8)',
+        gradient2: 'hsla(242, 74%, 61%, 0.8)'
       }
     },
   },
